3fabf8f65cb9a8cbfd4682fd5b1e485e0382cb0a
HermanSkop <armaton111@gmail.com>
2025-03-11 14:30:25 +0100

    Updated colors

--- src/app/dtos/product-preview.dto.ts
+++ src/app/dtos/product-preview.dto.ts
@@ -1,8 +1,10 @@
 export class ProductPreviewDto {
   constructor(
     public name: string,
+    public description: string,
     public price: number,
     public image: string | null,
+    public amountLeft: number,
     public categories: {id: number, name: string}[]
   ) {}
 }
